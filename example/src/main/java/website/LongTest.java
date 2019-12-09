53295a9ce4fe5a5eafdc046350d7d5807b64bb9b
LarsG <LarsG@lucanet.com>
2019-12-09 16:45:35 +0100

    added long range test case

---
+++ example/src/main/java/website/LongTest.java
@@ -0,0 +1,13 @@
+package website;
+
+/**
+ * TODO: DESC CLASS
+ *
+ * @author LarsG
+ */
+public class LongTest {
+  public static void main(String[] args) {
+    System.out.println(Long.MAX_VALUE);
+    System.out.println("9,223,372,036,854,775,807 " + Long.MAX_VALUE);
+  }
+}
